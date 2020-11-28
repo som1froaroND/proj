c59d8e0ce154eb30a75f94700893c391e8e504aa
som1froaroND <trendtechmaingamesclipcomil@gmail.com>
2020-11-28 18:33:40 +0200

    bottom navbar

--- app/src/main/java/com/foo/restaurantselection/MainActivity.kt
+++ app/src/main/java/com/foo/restaurantselection/MainActivity.kt
@@ -2,10 +2,25 @@ package com.foo.restaurantselection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.foo.restaurantselection.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // inflate layout
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+
+        //  set up  BottomNavigation
+        val navController = findNavController(R.id.myNavHostFragment)
+
+        //val bottomNav = binding.bottomNavigationView
+        val bottomNav = binding.bottomNavigationView
+        bottomNav?.setupWithNavController(navController)
+
     }
 }
